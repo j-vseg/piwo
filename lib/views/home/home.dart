4338408685fdacb92d7f81081b8dc395b8b379ef
katjaaa_ <vanseggelenjanne@gmail.com>
2025-01-24 16:30:50 +0100

    Fix bug - Long loading screen

--- lib/views/home/home.dart
+++ lib/views/home/home.dart
@@ -10,50 +10,29 @@ import 'package:piwo/widgets/notifiers/availablity_notifier.dart';
 import 'package:provider/provider.dart';
 import 'package:piwo/services/account.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  @override
-  HomePageState createState() => HomePageState();
-}
-
-class HomePageState extends State<HomePage> {
-  Account _profile = Account();
-  bool _isLoadingProfile = true;
-
-  @override
-  void initState() {
-    super.initState();
-    _fetchProfileInfo();
+  Future<Account> _fetchProfileInfo() async {
+    final accountService = AccountService();
+    final profile = (await accountService.getMyAccount()).data!;
+    return profile;
   }
 
-  void _fetchProfileInfo() async {
-    try {
-      final profile = (await AccountService().getMyAccount()).data!;
-      setState(() {
-        _profile = profile;
-        _isLoadingProfile = false;
-      });
-    } catch (e) {
-      setState(() {
-        _isLoadingProfile = false;
-      });
-    }
+  Future<List<Activity>> _fetchActivities(
+      ActivityProvider activityProvider) async {
+    final activities = activityProvider.activities
+        .where((activity) =>
+            DateTime.now().toLocal().isBefore(activity.endDate!.toLocal()))
+        .toList()
+      ..sort((a, b) => a.startDate!.compareTo(b.startDate!));
+    return activities.take(3).toList();
   }
 
   @override
   Widget build(BuildContext context) {
     return Consumer<ActivityProvider>(
       builder: (context, activityProvider, child) {
-        final activities = activityProvider.activities
-            .where((activity) =>
-                DateTime.now().toLocal().isBefore(activity.endDate!.toLocal()))
-            .toList()
-          ..sort((a, b) => a.startDate!.compareTo(b.startDate!));
-
-        List<Activity> limitedActivities =
-            activities.isNotEmpty ? activities.take(3).toList() : [];
-
         return CustomScaffold(
           useAppBar: true,
           appBar: AppBar(
@@ -68,62 +47,111 @@ class HomePageState extends State<HomePage> {
           backgroundColor: Colors.white,
           bodyPadding:
               const Padding(padding: EdgeInsets.symmetric(horizontal: 0.0)),
-          body: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  width: double.infinity,
-                  decoration: const BoxDecoration(
-                    color: CustomColors.themeBackground,
+          body: FutureBuilder<Account>(
+            future: _fetchProfileInfo(),
+            builder: (context, profileSnapshot) {
+              if (profileSnapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else if (profileSnapshot.hasError) {
+                return const Center(
+                  child: Text(
+                    'Er is een fout opgetreden. Probeer het later opnieuw.',
+                    style: TextStyle(color: Colors.red, fontSize: 16),
                   ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.only(
-                          top: 0.0,
-                          left: SizeSetter.getHorizontalScreenPadding(),
-                          right: SizeSetter.getHorizontalScreenPadding(),
+                );
+              } else if (profileSnapshot.hasData) {
+                final profile = profileSnapshot.data!;
+                return FutureBuilder<List<Activity>>(
+                  future: _fetchActivities(activityProvider),
+                  builder: (context, activitiesSnapshot) {
+                    if (activitiesSnapshot.connectionState ==
+                        ConnectionState.waiting) {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    } else if (activitiesSnapshot.hasError) {
+                      return const Center(
+                        child: Text(
+                          'Fout bij het ophalen van activiteiten.',
+                          style: TextStyle(color: Colors.red, fontSize: 16),
                         ),
-                        child: _isLoadingProfile
-                            ? const CircularProgressIndicator()
-                            : Text(
-                                "Welkom, ${_profile.firstName}!",
-                                style: const TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                ),
+                      );
+                    } else if (activitiesSnapshot.hasData) {
+                      final limitedActivities = activitiesSnapshot.data!;
+                      return SingleChildScrollView(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              width: double.infinity,
+                              decoration: const BoxDecoration(
+                                color: CustomColors.themeBackground,
                               ),
-                      ),
-                      const SizedBox(height: 20),
-                    ],
-                  ),
-                ),
-                limitedActivities.isEmpty
-                    ? CustomPaint(
-                        size: MediaQuery.of(context).size,
-                        painter: InvertedRoundedRectanglePainter(
-                          color: Colors.white,
-                          radius: 35,
-                          backgroundColor: CustomColors.themeBackground,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Padding(
+                                    padding: EdgeInsets.only(
+                                      top: 0.0,
+                                      left: SizeSetter
+                                          .getHorizontalScreenPadding(),
+                                      right: SizeSetter
+                                          .getHorizontalScreenPadding(),
+                                    ),
+                                    child: Text(
+                                      "Welkom, ${profile.firstName}!",
+                                      style: const TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                  const SizedBox(height: 20),
+                                ],
+                              ),
+                            ),
+                            limitedActivities.isEmpty
+                                ? CustomPaint(
+                                    size: MediaQuery.of(context).size,
+                                    painter: InvertedRoundedRectanglePainter(
+                                      color: Colors.white,
+                                      radius: 35,
+                                      backgroundColor:
+                                          CustomColors.themeBackground,
+                                    ),
+                                    child: const Padding(
+                                      padding: EdgeInsets.all(50.0),
+                                      child: Center(
+                                        child: Text(
+                                            'Geen toekomstige activiteiten beschikbaar.'),
+                                      ),
+                                    ),
+                                  )
+                                : ActivityOverview(
+                                    activities: limitedActivities,
+                                    account: profile,
+                                    title: "Toekomstige activiteiten",
+                                    description:
+                                        "Bekijk de meest recent toekomstige activiteiten.",
+                                  ),
+                          ],
                         ),
-                        child: const Padding(
-                          padding: EdgeInsets.all(50.0),
-                          child: Center(
-                            child: CircularProgressIndicator(),
-                          ),
-                        ),
-                      )
-                    : ActivityOverview(
-                        activities: limitedActivities,
-                        account: _profile,
-                        title: "Toekomstige activiteiten",
-                        description:
-                            "Bekijk de meest recent toekomstige activiteiten.",
-                      )
-              ],
-            ),
+                      );
+                    } else {
+                      return const Center(
+                        child: Text('Geen gegevens beschikbaar.'),
+                      );
+                    }
+                  },
+                );
+              } else {
+                return const Center(
+                  child: Text('Geen gegevens beschikbaar.'),
+                );
+              }
+            },
           ),
         );
       },
