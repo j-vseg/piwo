bda76a15c6daa9fcda274d5e3c2ada48414ff1ad
Janne van Seggelen <janne.vanseggelen@iodigital.com>
2026-02-13 14:41:47 +0100

    chore: ready ststic build

--- src/app/(authenticated)/activity/page.tsx
+++ src/app/(authenticated)/activity/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { LoadingIndicator } from "@/components/LoadingIndicator";
 import { ActivityPage } from "@/domians/activity/activity";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 function ActivityPageContent() {
   const searchParams = useSearchParams();
@@ -14,8 +16,10 @@ function ActivityPageContent() {
   return <ActivityPage id={id} />;
 }
 
-export default function Acitvity() {
+export default function Activity() {
   return (
+    <Suspense fallback={<LoadingIndicator />}>
       <ActivityPageContent />
+    </Suspense>
   );
 }
