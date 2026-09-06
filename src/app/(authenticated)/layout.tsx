107f325359746e76cf4e4bac8ed14e81e2fff30b
Janne van Seggelen <janne.vanseggelen@iodigital.com>
2026-09-06 11:20:42 +0200

    fix: verification bug

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -1,8 +1,12 @@
 "use client";
 
 import { BottomNavigation } from "@/components/BottomNavigation";
+import { LoadingIndicator } from "@/components/LoadingIndicator";
 import { WhatsNewOverlay } from "@/components/WhatsNewOverlay/WhatsNewOverlay";
+import { useAuth } from "@/contexts/auth";
 import { deletePastEvents } from "@/services/firebase/events";
+import { Approval } from "@/types/approval";
+import { useRouter } from "next/navigation";
 import { ReactNode, useEffect } from "react";
 
 export default function AuthenticatedLayout({
@@ -10,9 +14,26 @@ export default function AuthenticatedLayout({
 }: {
   children: ReactNode;
 }) {
+  const { user, approval, isLoading } = useAuth();
+  const { replace } = useRouter();
+
+  useEffect(() => {
+    if (!isLoading && (!user || approval !== Approval.Accepted)) {
+      replace("/");
+    }
+  }, [user, approval, isLoading, replace]);
+
   useEffect(() => {
     deletePastEvents().catch(console.error);
   }, []);
+
+  if (isLoading || !user || approval !== Approval.Accepted) {
+    return (
+      <div className="flex justify-center items-center min-h-[50vh]">
+        <LoadingIndicator />
+      </div>
+    );
+  }
 
   return (
     <>
